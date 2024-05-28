0552d4966e9fef65735e729e23caec6c2f61b161
Tarasdeveloper <107717399+Tarasdeveloper@users.noreply.github.com>
2024-05-28 18:49:45 +0000

    Deploying to gh-pages from @ Tarasdeveloper/js-homework_09@3deb52b75bb78aee068b886b095793436353ed9d 🚀

---
+++ 01-color-switcher.7a9721ab.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;t.addEventListener("click",(function(){r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.7a9721ab.js.map
