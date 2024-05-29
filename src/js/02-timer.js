35b358c9621edad50e3f032b54a63b5893e6985c
Tarasdeveloper <tomahavki1@gmail.com>
2024-05-29 23:04:23 +0300

    timer finished

---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const picker = document.querySelector('#datetime-picker');
+
+const timerBtn = document.querySelector('button[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+let selectedDate;
+timerBtn.setAttribute('disabled', true);
+timerBtn.addEventListener('click', onStartTimer);
+
+function onStartTimer() {
+  timerBtn.setAttribute('disabled', true);
+  picker.setAttribute('disabled', true);
+
+  timerId = setInterval(() => {
+    const pickedTime = selectedDate - Date.now();
+    showTime(convertMs(pickedTime));
+    if (pickedTime < 1000) {
+      Notiflix.Notify.success('Время закончилось!');
+      clearInterval(timerId);
+      picker.removeAttribute('disabled');
+    }
+  }, 1000);
+}
+
+flatpickr(picker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    if (selectedDate < Date.now()) {
+      timerBtn.setAttribute('disabled', true);
+      Notiflix.Notify.failure('Please choose a date in the future');
+      return;
+    }
+    timerBtn.removeAttribute('disabled');
+  },
+});
+
+function showTime({ days, hours, minutes, seconds }) {
+  dataDays.textContent = `${days}`;
+  dataHours.textContent = `${hours}`;
+  dataMinutes.textContent = `${minutes}`;
+  dataSeconds.textContent = `${seconds}`;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
